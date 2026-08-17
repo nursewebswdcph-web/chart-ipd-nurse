e63bfa2375c8fc0dc5a5bb51b7d948dbf18debca
Nurse Sawang <nurse.web.swdcph@gmail.com>
2026-08-17 09:02:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ipd-nurse-workbench-v20260815_03';
+const CACHE_NAME = 'ipd-nurse-workbench-v20260817_01';
 
 // Install: Skip waiting immediately to activate new SW
 self.addEventListener('install', (event) => {
